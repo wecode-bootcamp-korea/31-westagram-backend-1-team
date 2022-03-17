2a49d1fcc597051c470c45120fafb7127c1e5f75
고현영 <hyunyoungko@gohyeon-yeong-ui-MacBookAir.local>
2022-03-17 17:01:40 +0900

    ADD: user application의 models.py 수정

--- students/hyunyoung/users/models.py
+++ students/hyunyoung/users/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=45)
-    email = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
-    phone_name = models.CharField(max_length=45)
+    phone_number = models.CharField(max_length=45)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
