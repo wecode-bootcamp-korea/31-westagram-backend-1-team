aa4057424ffda99cf757bd99b883a615482ef52d
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-03-17 16:48:46 +0900

    생년월일, 계정생성일, 계정수정일 모델 작성

--- students/suhun/users/models.py
+++ students/suhun/users/models.py
@@ -1,12 +1,16 @@
+from turtle import update
+from venv import create
 from django.db import models
 
 
 class User(models.Model):
     name          = models.CharField(max_length=30)
-    email         = models.EmailField(max_length=245)
+    email         = models.EmailField(unique=True, max_length=245)
     password      = models.CharField(max_length=100)
-    phone         = models.IntegerField()
+    phone         = models.CharField(max_length=100)
     date_of_birth = models.DateField(auto_now_add=False,auto_now=False)
+    created_at    = models.DateField(auto_now_add=True, auto_now=False)  
+    update_at     = models.DateField(auto_now_add=True, auto_now= False)
     
     class Meta:
         db_table = "users"
