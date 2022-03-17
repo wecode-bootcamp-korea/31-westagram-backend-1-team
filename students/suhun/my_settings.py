2180cd50acf4568a48b6c10f1402909b8984e95b
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-03-17 15:15:42 +0900

    [ADD]: 초기 세팅

---
+++ students/suhun/my_settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westagram',
+        'USER': 'root',
+        'PASSWORD': '9292455z',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+     	'OPTIONS': {'charset': 'utf8mb4'}
+    }
+}   
+
+SECRET_KEY = 'django-insecure-=jzc+7a-wrt5@(+ot2vg=w516xn_x*3t^ygq3gh^lxq%tf)6k5'
