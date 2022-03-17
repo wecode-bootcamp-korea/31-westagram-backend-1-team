413de7d834a737f5cb46d15fe3c1be624a5f3375
DaeHyunKim <kimdaehyun10806@gmail.com>
2022-03-17 13:59:54 +0900

    [INIT] project initial setting

---
+++ students/daehyun/westargram/my_setting.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westargram',
+        'USER': 'root',
+        'PASSWORD': 'rlaeogus0116',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+     	'OPTIONS': {'charset': 'utf8mb4'}
+    }
+}
+        
+SECRET_KEY = 'django-insecure-c5^50%^ffwum9!l(u^guzww4_skjyv4w&)jycqoj&0kxo44)1@'
