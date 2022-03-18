d3e26a2e87bd3e715e003bb2a488756c5f4a7fee
고현영 <hyunyoungko@gohyeon-yeong-ui-MacBookAir.local>
2022-03-18 13:55:34 +0900

    ADD: 회원가입 view 추가

--- students/hyunyoung/users/views.py
+++ students/hyunyoung/users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+import re
+import json
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+from users.models import User
+
+class SignUpView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        
+        try:
+            email_form    = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+            password_form = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$")
+
+
+            if not email_form.match(data['email']):
+                return JsonResponse({'message':'EMAIL FORM ERROR'}, status=400)
+            if not password_form.match(data['password']):
+                return JsonResponse({'message':'PASSWORD FORM ERROR'}, status=400)
+          
+            users = User.objects.filter(email=data['email'])
+            if users:
+                return JsonResponse({'message':'this email already exists'}, status=400)
+                
+
+            User.objects.create(
+                name         = data['name'],
+                email        = data['email'],
+                password     = data['password'],
+                phone_number = data['phone'],
+            )
+
+            return JsonResponse({'message':'SUCCESS'}, status=201)
+
+        except:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
