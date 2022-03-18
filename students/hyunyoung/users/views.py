fbe86a87984b7a1bd3c08c80b6ab9ba042fa5e01
고현영 <hyunyoungko@gohyeon-yeong-ui-MacBookAir.local>
2022-03-18 16:52:30 +0900

    ADD: views.py KeyError 추가

--- students/hyunyoung/users/views.py
+++ students/hyunyoung/users/views.py
@@ -11,25 +11,24 @@ class SignUpView(View):
         data = json.loads(request.body)
         
         try:
-            email = data['email']
+            email    = data['email']
             password = data['password']
 
             email_form    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
             password_form = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
 
+            if not email and password:
+                raise KeyError
+
             if not re.match(email_form, email):
-                if email is '':
-                    return JsonResponse({'message':'EMAIL IS REQUIRED'}, status=400)
                 return JsonResponse({'message':'EMAIL FORM ERROR'}, status=400)
 
             if not re.match(password_form, data['password']):
-                if password is '':
-                    return JsonResponse({'message':'PASSWORD IS REQUIRED'}, status=400)
                 return JsonResponse({'message':'PASSWORD FORM ERROR'}, status=400)
             
             if User.objects.filter(email=email):
                 return JsonResponse({'message':'ALREADY EXIST'}, status=400)
-                
+        
             User.objects.create(
                 name         = data['name'],
                 email        = email,
@@ -38,6 +37,9 @@ class SignUpView(View):
             )
 
             return JsonResponse({'message':'SUCCESS'}, status=201)
+        
+        except KeyError:
+            return JsonResponse({'message':'ENTER EMAIL OR PASSWORD'}, status=400)
 
         except:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
