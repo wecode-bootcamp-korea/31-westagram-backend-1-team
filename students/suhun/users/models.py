c458d5c31ade48eaa203ceb7ba35aaf0e9f4b5e5
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-03-18 13:57:07 +0900

    git reset 후 필요 없는 정보 삭제 후 다시 commit

--- students/suhun/users/models.py
+++ students/suhun/users/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-
 class User(models.Model):
-    name        = models.CharField(max_length=30)
-    email       = models.EmailField(max_length=245)
-    password    = models.CharField(max_length=100)
-    phone       = models.IntegerField(max_length=120)
-
+    name          = models.CharField(max_length=30)
+    email         = models.EmailField(unique=True, max_length=245)
+    password      = models.CharField(max_length=100)
+    phone         = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    created_at    = models.DateTimeField(auto_now_add=True)  
+    update_at     = models.DateTimeField(auto_now= True)
+    
     class Meta:
         db_table = "users"
