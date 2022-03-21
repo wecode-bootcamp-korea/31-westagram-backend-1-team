ead5310ed9287d22ed298f99ef3672016c3b45cc
DaeHyunKim <kimdaehyun10806@gmail.com>
2022-03-21 13:30:36 +0900

    [ADD] SignIn feature

--- students/daehyun/westargram/users/views.py
+++ students/daehyun/westargram/users/views.py
@@ -17,7 +17,7 @@ class SignUpView(View):
             PASSWORD_RULE = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
             
             if User.objects.filter(email=email).exists():
-                return JsonResponse({'message' : "EMAIL_ALREADY_EXISTS"}, status = 400)
+                return JsonResponse({'message' : 'EMAIL_ALREADY_EXISTS'}, status = 400)
             
             if not re.match(EMAIL_RULE, email):
                 return JsonResponse ({'message' : 'INVALID_EMAIL'}, status = 400)
@@ -31,7 +31,34 @@ class SignUpView(View):
                 password     = password,
                 phone_number = data['phone_number']
             )
-            return JsonResponse({'message' : "SUCCESS"}, status = 201)
+            return JsonResponse({'message' : 'SUCCESS'}, status = 201)
                 
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
+        
+class SignInView(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            email    = data['email']
+            password = data['password']
+            
+            EMAIL_RULE    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+            PASSWORD_RULE = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
+
+            if not re.match(EMAIL_RULE, email):
+                return JsonResponse ({'message' : 'INVALID_USER_EMAIL'}, status = 400)
+            
+            if not re.match(PASSWORD_RULE, password):
+                return JsonResponse ({'message' : 'INVALID_USER_PASSWORD'}, status = 400)
+            
+            if not User.objects.filter(email=email).exists():
+                return JsonResponse({'message' : 'EMAIL_NOT_EXISTS'}, status = 400)
+            
+            if User.objects.get(email=email).password != password:
+                return JsonResponse({'message' : 'PASSWORD_INCORRECT'}, status = 400)
+            
+            return JsonResponse({'message' : 'SUCCESS'}, status = 201)
+                
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
