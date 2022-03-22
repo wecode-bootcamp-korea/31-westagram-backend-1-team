af82efa98518790bb4168c07f5ce215750426794
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-03-22 16:19:03 +0900

    [Modify]: 비밀번호 암호화

--- students/suhun/users/views.py
+++ students/suhun/users/views.py
@@ -1,4 +1,4 @@
-import json , re
+import json , re , bcrypt
 
 from django.http import JsonResponse
 from django.views import View
@@ -6,7 +6,7 @@ from django.views import View
 from .models import User
 
 class SignUpView(View):
-    def post(self, request): 
+    def post(self, request):
         try: 
             data = json.loads(request.body)
             name            = data['name']
@@ -14,6 +14,7 @@ class SignUpView(View):
             password        = data['password']
             phone           = data['phone']
             date_of_birth   = data['date_of_birth']
+            
             PASSWORD_CHECK  = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
             EMAIL_CHECK     = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
         
@@ -24,18 +25,22 @@ class SignUpView(View):
                 return JsonResponse({"massage":"Invalid e-mail format."}, status=401)
             
             if not re.match(PASSWORD_CHECK, password):
-                return JsonResponse({"massage":"Invalid password format."}, status=401)
+                return JsonResponse({"massage":"Invalid password format."}, status=401) 
             
+            new_salt = bcrypt.gensalt()
+            encoded_password     = password.encode('utf-8')
+            hashed_password      = bcrypt.hashpw(encoded_password , new_salt)
+            decode_hash_password = hashed_password.decode('utf-8')
             User.objects.create(
                         name            =  name,
                         email           =  email,
-                        password        =  password,
+                        password        =  decode_hash_password,
                         phone           =  phone,
                         date_of_birth   =  date_of_birth
                     )
-            return JsonResponse({'message':'success'}, status=201)
+            return JsonResponse({'message':'SUCCESS'}, status=201)
         except KeyError:
-            return JsonResponse({'message':'keyerror'}, status=401)
+            return JsonResponse({'message':'key_error'}, status=401)
 
 
 class SignInView(View):    
